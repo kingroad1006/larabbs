434e70c6197308e5638c7398595743ac78936085
kingroad1006 <632271044@qq.com>
2019-03-22 09:49:00 +0000

    用户最后活跃时间

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
 
     use Traits\ActiveUserHelper;
 
+    use Traits\LastActivedAtHelper;
+
     public function notify($instance)
     {
         // 如果要通知的人是当前用户，就不必通知了！
