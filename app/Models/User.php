1a189c74c427f14b25d29cf5959702f31c30c82f
kingroad1006 <632271044@qq.com>
2019-03-22 08:58:52 +0000

    活跃用户

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
         notify as protected laravelNotify;
     }
 
+    use Traits\ActiveUserHelper;
+
     public function notify($instance)
     {
         // 如果要通知的人是当前用户，就不必通知了！
